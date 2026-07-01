ed3659381ead3b2fe34f6bf75400c2ced5ab0b85
Gian Zanatelli <gianzanatelli@yahoo.com>
2026-07-01 00:37:14 -0300

    Aprendendo Modularização

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 **Let's Go!**
 
 > Aqui, a ideia é criar uma primeira interface para navegação, e depois, abas navegaveis de diferentes temas.
+
+# 28/06/2026
+
+> Aqui, preciso buscar aprender como faço minha página sair de um tópico do site e ir para outro.
+
+# **01/07/2026** *00:37*
+
+> Durante os dias 29/06 e 30/06, aprendi como funciona a navegação entre páginas e também sobre modularização, que é uma das práticas mais fundamentais e recomendadas do mercado. Sendo assim, pude fazer um exemplo para entender como funciona e aprender na prática.
+
+
--- index.php
+++ index.php
@@ -13,6 +13,30 @@ $projetos = [
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+<header>
+	<h1>cabeçalho</h1>
+</header>
 
+<div class="main">
+		<aside class="sidebar">
+			<nav>
+				<details>
+					<summary> Tecnology </summary>
+					
+					<ul>
+						<li><a href="games.php">Jogos</a></li>
+					</ul>
+				</details>
+			</nav>
+		</aside>
+	<main class="active-window">
+		<h1 style="color: green;">Gian <a style="color: white;">Valério </a><a style="color: red;">Zanatelli </a></h1>
+	</main>
+
+</div>
+
+<footer>
+	<h1 style="color:white;">rodapé</h1>
+</footer>
 </body>
 </html>
